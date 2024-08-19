9979d381c0f2f65f3c8a0f0bc02ea3ff69b023a0
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-08-18 22:29:21 -0400

    Criação do Projeto (Laravel + React + Bootstrap)

---
+++ resources/js/Pages/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import Titulo from "@/Components/Titulo";
+import Container from "@/Layouts/Container";
+
+export default function HelloWorld() {
+    return (
+        <Container>
+            <Titulo />
+        </Container>
+    );
+}
