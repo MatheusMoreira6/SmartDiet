43f3dd5227b29db508cbbfeb9ca985a0d5aaebcc
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-09-16 20:27:30 -0400

    Removido Arquivos de Exemplo

--- resources/js/Pages/HelloWorld.jsx
+++ resources/js/Pages/HelloWorld.jsx
@@ -1,10 +1,12 @@
-import Titulo from "@/Components/Titulo";
 import Container from "@/Layouts/Container";
+import { Head } from "@inertiajs/react";
 
 export default function HelloWorld() {
     return (
         <Container>
-            <Titulo />
+            <Head title="Hello World" />
+
+            <h1>Hello World</h1>
         </Container>
     );
 }
