a0f9263ab0bb4a34ba7deecd33df894fc7498659
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-18 22:25:50 -0400

    Refactorizar manipulação do padding no componente ContainerFluid

--- resources/js/Components/ContainerFluid.jsx
+++ resources/js/Components/ContainerFluid.jsx
@@ -1,5 +1,5 @@
-const ContainerFluid = ({ children }) => {
-    return <div className="container-fluid py-4">{children}</div>;
+const ContainerFluid = ({ children, padding = "py-0" }) => {
+    return <div className={`container-fluid ${padding}`}>{children}</div>;
 };
 
 export default ContainerFluid;
