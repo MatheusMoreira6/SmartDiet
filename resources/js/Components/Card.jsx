834c72d42ed5e08f962dfd24d3189dc760630e5d
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-18 20:47:10 -0400

    Refatorar componente Card adicionando suporte a onClick

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -1,22 +1,39 @@
-const Card = ({ title, children, footer }) => {
+const Card = ({ title, children, footer, onClick }) => {
+    const renderHeader = () => {
+        if (!title) return null;
+
+        return (
+            <div className="card-header">
+                <h5 className="text-center">{title}</h5>
+            </div>
+        );
+    };
+
+    const renderBody = () => {
+        if (!title && !footer) return children;
+
+        return <div className="card-body">{children}</div>;
+    };
+
+    const renderFooter = () => {
+        if (!footer) return null;
+
+        return (
+            <div className="card-footer">
+                <p className="text-center my-2">{footer}</p>
+            </div>
+        );
+    };
+
     return (
         <div
+            onClick={onClick}
             className="card m-auto"
             style={{ width: "280px", height: "350px" }}
         >
-            {title && (
-                <div className="card-header">
-                    <h5 className="text-center">{title}</h5>
-                </div>
-            )}
-
-            <div className="card-body">{children}</div>
-
-            {footer && (
-                <div className="card-footer">
-                    <p className="text-center my-2">{footer}</p>
-                </div>
-            )}
+            {renderHeader()}
+            {renderBody()}
+            {renderFooter()}
         </div>
     );
 };
