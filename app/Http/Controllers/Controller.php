b6c4dbb2fa79607837a5bee98302da2d009f86ab
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-19 19:15:17 -0400

    refactor: Atualizar Controller com métodos auxiliares
    Atualizei o arquivo Controller.php para adicionar métodos auxiliares que facilitam a renderização de componentes, o redirecionamento para rotas e o tratamento de erros de redirecionamento.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,12 +8,43 @@ use Inertia\Inertia;
 
 abstract class Controller
 {
+    /**
+     * Renderiza um componente com as propriedades fornecidas.
+     *
+     * @param string $component O nome do componente a ser renderizado.
+     * @param array $props Propriedades adicionais para passar ao componente.
+     * @return \Inertia\Response
+     */
     protected function render($component, $props = [])
     {
+        $props = array_merge($props, session('props', []));
+
         return Inertia::render($component, array_merge($props, [
-            'csrf_token' => csrf_token(),
             'user' =>  Auth::user(),
             'currentRoute' => Route::currentRouteName(),
         ]));
+    }
+
+    /**
+     * Redireciona para uma rota com as propriedades fornecidas.
+     *
+     * @param string $route O nome da rota para redirecionar.
+     * @param array $props Propriedades adicionais para passar com o redirecionamento.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function response($route, $props = [])
+    {
+        redirect()->route($route)->with('props', $props);
+    }
+
+    /**
+     * Redireciona de volta com mensagens de erro.
+     *
+     * @param array $props Mensagens de erro para passar com o redirecionamento.
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function responseErrors($props = [])
+    {
+        redirect()->back()->withErrors($props);
     }
 }
