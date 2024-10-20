a1d7e05ddf4dfeec019740d96562e0706d43ad45
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-19 22:13:21 -0400

    feat: Adicionar métodos auxiliares para exibir alertas personalizados
    Adiciona métodos auxiliares para exibir alertas personalizados de erro, sucesso, informação e aviso utilizando a biblioteca SweetAlert.

--- resources/js/Components/SweetAlert.jsx
+++ resources/js/Components/SweetAlert.jsx
@@ -3,4 +3,36 @@ import withReactContent from "sweetalert2-react-content";
 
 const SweetAlert = withReactContent(Swal);
 
+SweetAlert.error = ({ title, text }) => {
+    return SweetAlert.fire({
+        icon: 'error',
+        title: title,
+        text: text
+    });
+};
+
+SweetAlert.success = ({ title, text }) => {
+    return SweetAlert.fire({
+        icon: 'success',
+        title: title,
+        text: text
+    });
+};
+
+SweetAlert.info = ({ title, text }) => {
+    return SweetAlert.fire({
+        icon: 'info',
+        title: title,
+        text: text
+    });
+};
+
+SweetAlert.warning = ({ title, text }) => {
+    return SweetAlert.fire({
+        icon: 'warning',
+        title: title,
+        text: text
+    });
+};
+
 export default SweetAlert;
