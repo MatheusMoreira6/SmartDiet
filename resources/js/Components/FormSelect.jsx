1e5a6e488f6d6575bcb9f6fe90ada85656e56231
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-20 19:31:24 -0400

    refactor: Refeito o componente FormSelect em React Bootstrap

---
+++ resources/js/Components/FormSelect.jsx
@@ -0,0 +1,47 @@
+import { Form } from "react-bootstrap";
+
+const FormSelect = ({
+    id,
+    label,
+    options,
+    value,
+    autoFocus,
+    required,
+    isInvalid,
+    onChange,
+    textError,
+}) => {
+    const selectOptions = [{ id: "", descricao: "Selecione..." }, ...options];
+
+    return (
+        <Form.Group controlId={id}>
+            <Form.Label>{label}</Form.Label>
+
+            <Form.Select
+                value={value}
+                autoFocus={autoFocus}
+                isInvalid={isInvalid}
+                onChange={onChange}
+                required={required}
+            >
+                {selectOptions.map((option, index) => (
+                    <option
+                        key={index}
+                        value={option.id}
+                        disabled={option.id === ""}
+                    >
+                        {option.descricao}
+                    </option>
+                ))}
+            </Form.Select>
+
+            {textError ? (
+                <Form.Control.Feedback type="invalid">
+                    {textError}
+                </Form.Control.Feedback>
+            ) : null}
+        </Form.Group>
+    );
+};
+
+export default FormSelect;
