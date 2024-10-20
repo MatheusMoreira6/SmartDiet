e38eba7da63312eca0e07d9c2af3e3d93c1e199b
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-20 16:53:02 -0400

    refactor: Reformulado o componente FormInput para utilizar o React Bootstrap

---
+++ resources/js/Components/FormInput.jsx
@@ -0,0 +1,43 @@
+import InputMask from "react-input-mask";
+import Form from "react-bootstrap/Form";
+
+const FormInput = ({
+    id,
+    label,
+    type,
+    mask,
+    value,
+    autoFocus,
+    placeHolder,
+    isInvalid,
+    onChange,
+    textError,
+}) => {
+    return (
+        <>
+            <Form.Group controlId={id}>
+                <Form.Label>{label}</Form.Label>
+
+                <Form.Control
+                    as={InputMask}
+                    type={type}
+                    mask={mask}
+                    value={value}
+                    autoFocus={autoFocus}
+                    placeholder={placeHolder}
+                    isInvalid={isInvalid}
+                    onChange={onChange}
+                    required={textError ? true : false}
+                />
+
+                {textError ? (
+                    <Form.Control.Feedback type="invalid">
+                        {textError}
+                    </Form.Control.Feedback>
+                ) : null}
+            </Form.Group>
+        </>
+    );
+};
+
+export default FormInput;
