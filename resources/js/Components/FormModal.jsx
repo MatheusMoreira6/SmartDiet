7ea10c55e11c96d9b9a035c88625cf1030004104
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-21 17:02:34 -0400

    refactor: Refatorado componente modal para React Bootstrap

--- resources/js/Components/FormModal.jsx
+++ resources/js/Components/FormModal.jsx
@@ -1,121 +1,41 @@
-import Form from "./Form";
-import { Modal } from "bootstrap";
-import { useEffect } from "react";
-import ContainerFluid from "./ContainerFluid";
-import ButtonPrimary from "./ButtonPrimary";
-import ButtonDanger from "./ButtonDanger";
-
-const ModalClose = ({ performClose }) => {
-    return (
-        <button
-            type="button"
-            aria-label="Fechar"
-            className="btn-close"
-            onClick={performClose}
-        ></button>
-    );
-};
-
-const ModalSize = ({ size }) => {
-    let modalSize = "";
-
-    if (size === "sm") {
-        modalSize = "modal-sm";
-    } else if (size === "lg") {
-        modalSize = "modal-lg";
-    } else if (size === "xl") {
-        modalSize = "modal-xl";
-    }
-
-    return modalSize;
-};
+import { Button, Container, Modal } from "react-bootstrap";
 
 const FormModal = ({
+    show,
     title,
     children,
-    modalRef,
-    modalOpen,
-    formRef,
-    processing,
+    handleClose,
     handleSubmit,
-    performClose,
+    processing,
     size = "lg",
 }) => {
-    const modalSize = ModalSize({ size });
-
-    useEffect(() => {
-        const modal = modalRef.current;
-
-        if (modal) {
-            const modalInstance = Modal.getOrCreateInstance(modalRef.current);
-
-            if (modalOpen) {
-                modalInstance.show();
-            } else {
-                modalInstance.hide();
-
-                const form = formRef.current;
-
-                if (form) {
-                    formRef.current.reset();
-                    formRef.current.classList.remove("was-validated");
-                }
-            }
-
-            return () => {
-                modalInstance.dispose();
-            };
-        }
-    }, [modalOpen, modalRef]);
-
-    const performSubmit = () => {
-        const form = formRef.current;
-
-        if (form) {
-            formRef.current.dispatchEvent(
-                new Event("submit", { bubbles: true, cancelable: true })
-            );
-        }
-    };
-
     return (
-        <div
-            tabIndex="-1"
-            ref={modalRef}
-            className="modal fade"
-            data-bs-backdrop="static"
-            data-bs-keyboard="false"
+        <Modal
+            size={size}
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+            scrollable={true}
         >
-            <div
-                className={`modal-dialog modal-dialog-scrollable ${modalSize}`}
-            >
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">{title}</h5>
-                        <ModalClose performClose={performClose} />
-                    </div>
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
 
-                    <div className="modal-body">
-                        <Form formRef={formRef} handleSubmit={handleSubmit}>
-                            <ContainerFluid>{children}</ContainerFluid>
-                        </Form>
-                    </div>
+            <Modal.Body>
+                <Container fluid>{children}</Container>
+            </Modal.Body>
 
-                    <div className="modal-footer">
-                        <ButtonDanger onClick={performClose}>
-                            Cancelar
-                        </ButtonDanger>
+            <Modal.Footer>
+                <Button variant="danger" onClick={handleClose}>
+                    Cancelar
+                </Button>
 
-                        <ButtonPrimary
-                            disabled={processing}
-                            onClick={performSubmit}
-                        >
-                            {processing ? "Salvando..." : "Salvar"}
-                        </ButtonPrimary>
-                    </div>
-                </div>
-            </div>
-        </div>
+                <Button variant="primary" onClick={handleSubmit}>
+                    {processing ? "Salvando..." : "Salvar"}
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
