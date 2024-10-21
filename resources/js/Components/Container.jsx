a902cbef73012551f8055ae0689e6ab655419a46
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-21 01:26:18 -0400

    style: Ajustado margin e padding do container

--- resources/js/Components/Container.jsx
+++ resources/js/Components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
     return (
-        <div className="container rounded shadow-lg my-5 py-3">{children}</div>
+        <div className="container rounded shadow-lg mt-5 p-3">{children}</div>
     );
 };
 
