2c84cd37536f9bcde2e8fe2f01637c8c64b1e5ab
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-21 22:56:53 -0400

    fix: Corrigido modal de cadastro do paciente

--- resources/js/Pages/Admin/Pacientes.jsx
+++ resources/js/Pages/Admin/Pacientes.jsx
@@ -6,7 +6,7 @@ import FormInput from "@/Components/FormInput";
 import FormModal from "@/Components/FormModal";
 import FormSelect from "@/Components/FormSelect";
 import SweetAlert from "@/Components/SweetAlert";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Pacientes = ({ generos, pacientes }) => {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -19,11 +19,16 @@ const Pacientes = ({ generos, pacientes }) => {
         email: "",
     });
 
-    const [validated, setValidated] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    const [validated, setValidated] = useState(false);
 
     const handleShow = () => setShowModal(true);
-    const handleClose = () => setShowModal(false);
+
+    const handleClose = () => {
+        setShowModal(false);
+        setValidated(false);
+        reset();
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -36,13 +41,9 @@ const Pacientes = ({ generos, pacientes }) => {
                     const title = page.props.title;
                     const text = page.props.text;
 
-                    SweetAlert.success({ title: title, text: text }).then(
-                        () => {
-                            setShowModal(false);
-                        }
+                    SweetAlert.success({ title: title, text: text }).then(() =>
+                        handleClose()
                     );
-
-                    reset();
                 },
                 onError: () => {
                     errors.error && SweetAlert.error({ title: errors.error });
@@ -87,140 +88,141 @@ const Pacientes = ({ generos, pacientes }) => {
             <FormModal
                 show={showModal}
                 title={"Cadastrar Paciente"}
+                validated={validated}
+                processing={processing}
                 handleClose={handleClose}
                 handleSubmit={handleSubmit}
-                processing={processing}
             >
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Row className="g-3">
-                        <Col xs={12} lg={4}>
-                            <FormInput
-                                id={"nome"}
-                                label={"Nome"}
-                                type={"text"}
-                                value={data.nome}
-                                autoFocus={true}
-                                placeHolder={"Digite seu nome"}
-                                required={true}
-                                isInvalid={errors.nome}
-                                onChange={(e) =>
-                                    setData("nome", e.target.value)
-                                }
-                                textError={errors.nome ?? "Informe seu nome"}
-                            />
-                        </Col>
+                <Row className="g-3">
+                    <Col xs={12} lg={4}>
+                        <FormInput
+                            id={"nome"}
+                            label={"Nome"}
+                            type={"text"}
+                            value={data.nome}
+                            autoFocus={true}
+                            placeHolder={"Nome do paciente"}
+                            required={true}
+                            isInvalid={errors.nome}
+                            onChange={(e) => setData("nome", e.target.value)}
+                            textError={
+                                errors.nome ?? "Informe o nome do paciente"
+                            }
+                        />
+                    </Col>
 
-                        <Col xs={12} lg={4}>
-                            <FormInput
-                                id={"sobrenome"}
-                                label={"Sobrenome"}
-                                type={"text"}
-                                value={data.sobrenome}
-                                placeHolder={"Digite seu sobrenome"}
-                                required={true}
-                                isInvalid={errors.sobrenome}
-                                onChange={(e) =>
-                                    setData("sobrenome", e.target.value)
-                                }
-                                textError={
-                                    errors.sobrenome ?? "Informe seu sobrenome"
-                                }
-                            />
-                        </Col>
+                    <Col xs={12} lg={4}>
+                        <FormInput
+                            id={"sobrenome"}
+                            label={"Sobrenome"}
+                            type={"text"}
+                            value={data.sobrenome}
+                            placeHolder={"Sobrenome do paciente"}
+                            required={true}
+                            isInvalid={errors.sobrenome}
+                            onChange={(e) =>
+                                setData("sobrenome", e.target.value)
+                            }
+                            textError={
+                                errors.sobrenome ??
+                                "Informe o sobrenome do paciente"
+                            }
+                        />
+                    </Col>
 
-                        <Col xs={12} lg={4}>
-                            <FormInput
-                                id={"data_nascimento"}
-                                label={"Data de Nascimento"}
-                                type={"text"}
-                                mask={"99/99/9999"}
-                                value={data.data_nascimento}
-                                placeHolder={"__/__/____"}
-                                required={true}
-                                isInvalid={errors.data_nascimento}
-                                onChange={(e) =>
-                                    setData("data_nascimento", e.target.value)
-                                }
-                                textError={
-                                    errors.data_nascimento ??
-                                    "Informe sua data de nascimento"
-                                }
-                            />
-                        </Col>
+                    <Col xs={12} lg={4}>
+                        <FormInput
+                            id={"data_nascimento"}
+                            label={"Data de Nascimento"}
+                            type={"text"}
+                            mask={"99/99/9999"}
+                            value={data.data_nascimento}
+                            placeHolder={"__/__/____"}
+                            required={true}
+                            isInvalid={errors.data_nascimento}
+                            onChange={(e) =>
+                                setData("data_nascimento", e.target.value)
+                            }
+                            textError={
+                                errors.data_nascimento ??
+                                "Informe a data de nascimento do paciente"
+                            }
+                        />
+                    </Col>
 
-                        <Col xs={12} lg={4}>
-                            <FormSelect
-                                id={"sexo"}
-                                label={"Sexo"}
-                                options={generos}
-                                value={data.genero_id}
-                                required={true}
-                                isInvalid={errors.genero_id}
-                                onChange={(e) =>
-                                    setData("genero_id", e.target.value)
-                                }
-                                textError={
-                                    errors.genero_id ?? "Informe seu sexo"
-                                }
-                            />
-                        </Col>
+                    <Col xs={12} lg={4}>
+                        <FormSelect
+                            id={"sexo"}
+                            label={"Sexo"}
+                            options={generos}
+                            value={data.genero_id}
+                            required={true}
+                            isInvalid={errors.genero_id}
+                            onChange={(e) =>
+                                setData("genero_id", e.target.value)
+                            }
+                            textError={errors.genero_id ?? "Sexo do paciente"}
+                        />
+                    </Col>
 
-                        <Col xs={12} lg={4}>
-                            <FormInput
-                                id={"cpf"}
-                                label={"CPF"}
-                                type={"text"}
-                                mask={"999.999.999-99"}
-                                value={data.cpf}
-                                placeHolder={"Digite seu CPF"}
-                                required={true}
-                                isInvalid={errors.cpf}
-                                onChange={(e) => setData("cpf", e.target.value)}
-                                textError={errors.cpf ?? "Informe seu CPF"}
-                            />
-                        </Col>
+                    <Col xs={12} lg={4}>
+                        <FormInput
+                            id={"cpf"}
+                            label={"CPF"}
+                            type={"text"}
+                            mask={"999.999.999-99"}
+                            value={data.cpf}
+                            placeHolder={"999.999.999-99"}
+                            required={true}
+                            isInvalid={errors.cpf}
+                            onChange={(e) => setData("cpf", e.target.value)}
+                            textError={
+                                errors.cpf ?? "Informe o CPF do paciente"
+                            }
+                        />
+                    </Col>
 
-                        <Col xs={12} lg={4}>
-                            <FormInput
-                                id={"telefone"}
-                                label={"Telefone"}
-                                type={"text"}
-                                mask={"(99) 99999-9999"}
-                                value={data.telefone}
-                                placeHolder={"Digite seu telefone"}
-                                required={true}
-                                isInvalid={errors.telefone}
-                                onChange={(e) =>
-                                    setData("telefone", e.target.value)
-                                }
-                                textError={
-                                    errors.telefone ?? "Informe seu telefone"
-                                }
-                            />
-                        </Col>
+                    <Col xs={12} lg={4}>
+                        <FormInput
+                            id={"telefone"}
+                            label={"Telefone"}
+                            type={"text"}
+                            mask={"(99) 99999-9999"}
+                            value={data.telefone}
+                            placeHolder={"(99) 99999-9999"}
+                            required={true}
+                            isInvalid={errors.telefone}
+                            onChange={(e) =>
+                                setData("telefone", e.target.value)
+                            }
+                            textError={
+                                errors.telefone ??
+                                "Informe o telefone do paciente"
+                            }
+                        />
+                    </Col>
 
-                        <Col xs={12}>
-                            <FormInput
-                                id={"email"}
-                                label={"E-mail"}
-                                type={"email"}
-                                value={data.email}
-                                placeHolder={"Digite seu e-mail"}
-                                required={true}
-                                isInvalid={errors.email}
-                                onChange={(e) =>
-                                    setData(
-                                        "email",
-                                        e.target.value.toLowerCase()
-                                    )
-                                }
-                                textError={
-                                    errors.email ?? "Insira um e-mail válido"
-                                }
-                            />
-                        </Col>
-                    </Row>
-                </Form>
+                    <Col xs={12}>
+                        <FormInput
+                            id={"email"}
+                            label={"E-mail"}
+                            type={"email"}
+                            value={data.email}
+                            placeHolder={"E-mail do paciente"}
+                            required={true}
+                            isInvalid={errors.email}
+                            onChange={(e) =>
+                                setData("email", e.target.value.toLowerCase())
+                            }
+                            textError={
+                                errors.email ??
+                                (data.email
+                                    ? "Insira um e-mail válido"
+                                    : "Informe o e-mail do paciente")
+                            }
+                        />
+                    </Col>
+                </Row>
             </FormModal>
         </AdminLayout>
     );
