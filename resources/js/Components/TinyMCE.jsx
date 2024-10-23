1bedf58e11572f9e4b9c2ca6bc14e29b7fe3319a
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-23 00:04:52 -0400

    feat: Adicionar suporte ao TinyMCE para edição de texto
    Adiciona suporte ao TinyMCE para a edição de texto em um componente chamado TinyMCE.

---
+++ resources/js/Components/TinyMCE.jsx
@@ -0,0 +1,24 @@
+import { usePage } from "@inertiajs/react";
+import { Editor } from "@tinymce/tinymce-react";
+
+const TinyMCE = ({ value, onChange }) => {
+    const { tinymceApiKey } = usePage().props;
+
+    return (
+        <Editor
+            apiKey={tinymceApiKey}
+            init={{
+                height: 300,
+                menubar: false,
+                plugins: ["lists", "advlist", "autoresize"],
+                toolbar: `undo redo | bold italic | fontselect fontsizeselect | alignleft aligncenter alignright | bullist numlist`,
+                font_formats: `Arial=arial,helvetica,sans-serif; Courier New=courier new,courier,monospace; Times New Roman=times new roman,times,serif;`,
+                fontsize_formats: "10pt 12pt 14pt 18pt 24pt 36pt",
+            }}
+            initialValue={value}
+            onEditorChange={onChange}
+        />
+    );
+};
+
+export default TinyMCE;
