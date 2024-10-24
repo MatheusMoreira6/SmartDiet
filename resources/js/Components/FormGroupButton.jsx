c3f072a9a3183570f5b2f1b38e1dac13a446ed09
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-24 01:25:36 -0400

    feat: Adicionado componente FormGroupButton

---
+++ resources/js/Components/FormGroupButton.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+
+const FormGroupButton = ({
+    id,
+    label,
+    type,
+    bold,
+    value,
+    autoFocus,
+    placeHolder,
+    buttonText,
+    buttonVariant,
+    required,
+    isInvalid,
+    onChange,
+    onButtonClick,
+    textError,
+}) => {
+    return (
+        <Form.Group controlId={id}>
+            <Form.Label className={bold ? "fw-semibold" : ""}>
+                {label}
+            </Form.Label>
+
+            <InputGroup>
+                <Form.Control
+                    type={type}
+                    value={value}
+                    autoFocus={autoFocus}
+                    placeholder={placeHolder}
+                    required={required}
+                    isInvalid={isInvalid}
+                    onChange={onChange}
+                />
+
+                <Button variant={buttonVariant} onClick={onButtonClick}>
+                    {buttonText}
+                </Button>
+            </InputGroup>
+
+            {textError ? (
+                <Form.Control.Feedback type="invalid">
+                    {textError}
+                </Form.Control.Feedback>
+            ) : null}
+        </Form.Group>
+    );
+};
+
+export default FormGroupButton;
