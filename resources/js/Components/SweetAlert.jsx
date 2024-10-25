cbb5a95c37bd0a5df4abc7361c6ba7dea296dfc6
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-10-24 20:24:58 -0400

    feat: Atualizar componente SweetAlert
    Atualiza o componente SweetAlert para adicionar a funcionalidade de confirmação com botões personalizados.

--- resources/js/Components/SweetAlert.jsx
+++ resources/js/Components/SweetAlert.jsx
@@ -5,33 +5,49 @@ const SweetAlert = withReactContent(Swal);
 
 SweetAlert.error = ({ title, text }) => {
     return SweetAlert.fire({
-        icon: 'error',
+        icon: "error",
         title: title,
-        text: text
+        text: text,
     });
 };
 
 SweetAlert.success = ({ title, text }) => {
     return SweetAlert.fire({
-        icon: 'success',
+        icon: "success",
         title: title,
-        text: text
+        text: text,
     });
 };
 
 SweetAlert.info = ({ title, text }) => {
     return SweetAlert.fire({
-        icon: 'info',
+        icon: "info",
         title: title,
-        text: text
+        text: text,
     });
 };
 
 SweetAlert.warning = ({ title, text }) => {
     return SweetAlert.fire({
-        icon: 'warning',
+        icon: "warning",
         title: title,
-        text: text
+        text: text,
+    });
+};
+
+SweetAlert.confirm = ({
+    title,
+    text,
+    confirmButton = "Sim",
+    cancelButton = "Não",
+}) => {
+    return SweetAlert.fire({
+        title: title,
+        text: text,
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: confirmButton,
+        cancelButtonText: cancelButton,
     });
 };
 
