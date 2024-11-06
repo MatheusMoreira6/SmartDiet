538598e6170d527855bd4eb1418430227cedb646
Danilo Silva de Lima <802.245@alunos.unigran.br>
2024-11-05 20:10:48 -0400

    feat: adicioando notas no card

--- resources/js/Pages/Admin/Pacientes/DiarioAlimentar/DiarioAlimentar.jsx
+++ resources/js/Pages/Admin/Pacientes/DiarioAlimentar/DiarioAlimentar.jsx
@@ -1,6 +1,7 @@
 import { Card, Col, Row } from "react-bootstrap";
 
 export default function RenderFotos({ fotos = [] }) {
+    console.log(fotos);
     return (
         <Row>
             {fotos.map((foto) => (
@@ -16,6 +17,7 @@ export default function RenderFotos({ fotos = [] }) {
                                 Data:{" "}
                                 {new Date(foto.created_at).toLocaleDateString()}
                             </Card.Text>
+                            <p>{foto.notas}</p>
                         </Card.Body>
                     </Card>
                 </Col>
