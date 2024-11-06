a0c3de4afce88565b85e21fe73201aa2c96eb7f8
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2024-11-06 05:26:08 -0400

    refactor: Remover importações desnecessárias do flatpickr no app.jsx e package.json

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,5 @@
 import './bootstrap';
 import '../css/app.css';
-import "flatpickr/dist/flatpickr.min.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
