67a47c936ffcc06fe0be8e9f8208ae2b2400fc45
Danilo Silva de Lima <802.245@alunos.unigran.br>
2024-11-09 19:19:37 -0400

    feat: cadastro e remocao de grupo de dias

--- resources/js/Pages/Admin/Pacientes/Dietas/Dietas.jsx
+++ resources/js/Pages/Admin/Pacientes/Dietas/Dietas.jsx
@@ -5,9 +5,12 @@ import Api from "@/Api";
 import "../../../../../css/tableDieta.css";
 import { router } from "@inertiajs/react";
 import ModalEditDia from "./ModalEditDia";
+import { ModalCadastroGrupo } from "./ModalCadastroGrupo";
+import SweetAlert from "@/Components/SweetAlert";
 
 const DietContainer = ({ dietas, id_paciente, id_nutricionista }) => {
     const [show, setShow] = useState(false);
+    const [addGrupo, setAddGrupo] = useState(false);
 
     const [dietasDynamic, setDietas] = useState(dietas);
     const [diasSemana, setDias] = useState([]);
@@ -53,10 +56,50 @@ const DietContainer = ({ dietas, id_paciente, id_nutricionista }) => {
         setShowEditModal(true);
     };
 
+    async function handleDelete({ dieta_id, dia_id }) {
+        try {
+            const result = await SweetAlert.confirm({
+                title: "Você tem certeza?",
+                text: "Essa ação irá remover a dieta permanentemente.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Sim, remover!",
+                cancelButtonText: "Cancelar",
+            });
+
+            if (result.isConfirmed) {
+                const response = await Api.post(route("delete.grupo"), {
+                    dieta_id,
+                    dia_id,
+                    id_paciente,
+                    id_nutricionista,
+                });
+
+                setDietas(response.data.dietas);
+                SweetAlert.success({
+                    title: "Dieta removida com sucesso!",
+                });
+            }
+        } catch (error) {
+            SweetAlert.error({
+                title: "Erro ao remover dieta!",
+            });
+        }
+    }
+
     return (
         <Row>
             {dietasDynamic.length > 0 ? (
                 <>
+                    <Col md={12} style={{ marginBottom: 15 }}>
+                        <Button
+                            variant="primary"
+                            onClick={() => setAddGrupo(true)}
+                        >
+                            <i className="bi bi-plus-lg"></i>
+                            Cadastrar grupo
+                        </Button>
+                    </Col>
                     {diasSemana.map((dia) => (
                         <Card key={dia.id} className="mb-2 card-hover">
                             <Card.Body>
@@ -93,11 +136,32 @@ const DietContainer = ({ dietas, id_paciente, id_nutricionista }) => {
                                             <span>Ver Dieta </span>
                                             <i className="bi bi-arrow-right-circle"></i>
                                         </Button>
+                                        <Button
+                                            variant="danger"
+                                            onClick={() =>
+                                                handleDelete({
+                                                    dieta_id:
+                                                        dietasDynamic[0].id,
+                                                    dia_id: dia.id,
+                                                })
+                                            }
+                                        >
+                                            <i className="bi bi-trash"></i>
+                                        </Button>
                                     </Col>
                                 </Row>
                             </Card.Body>
                         </Card>
                     ))}
+                    <ModalCadastroGrupo
+                        visible={addGrupo}
+                        setShow={setAddGrupo}
+                        setDietas={setDietas}
+                        id_paciente={id_paciente}
+                        id_nutricionista={id_nutricionista}
+                        dieta_id={dietasDynamic[0].id}
+                        ordem={diasSemana.length}
+                    />
                 </>
             ) : (
                 <>
